eaafc24b28836afc04ebb277457bf458a351550a
huid <iamhuide@gmail.com>
2023-08-30 06:32:26 +0000

    init setup code

--- php_wcl.h
+++ php_wcl.h
@@ -1,15 +1,23 @@
 /* wcl extension for PHP */
 
 #ifndef PHP_WCL_H
-# define PHP_WCL_H
+#define PHP_WCL_H
 
 extern zend_module_entry wcl_module_entry;
-# define phpext_wcl_ptr &wcl_module_entry
+#define phpext_wcl_ptr &wcl_module_entry
 
-# define PHP_WCL_VERSION "0.1.0"
+#define PHP_WCL_VERSION "0.1.0"
 
-# if defined(ZTS) && defined(COMPILE_DL_WCL)
-ZEND_TSRMLS_CACHE_EXTERN()
-# endif
+#if defined(ZTS) && defined(COMPILE_DL_WCL)
+    ZEND_TSRMLS_CACHE_EXTERN()
+#endif
 
-#endif	/* PHP_WCL_H */
+ZEND_BEGIN_MODULE_GLOBALS(wcl)
+    zend_long filter_blank;
+ZEND_END_MODULE_GLOBALS(wcl)
+
+ZEND_EXTERN_MODULE_GLOBALS(wcl)
+
+#define WCL_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(wcl, v)
+
+#endif /* PHP_WCL_H */
--- wcl.c
+++ wcl.c
@@ -1,19 +1,27 @@
 /* wcl extension for PHP */
 
 #ifdef HAVE_CONFIG_H
-# include "config.h"
+#include "config.h"
 #endif
-
 #include "php.h"
+#include "php_ini.h"
 #include "ext/standard/info.h"
 #include "php_wcl.h"
 
+ZEND_DECLARE_MODULE_GLOBALS(wcl)
+
 /* For compatibility with older PHP versions */
 #ifndef ZEND_PARSE_PARAMETERS_NONE
-#define ZEND_PARSE_PARAMETERS_NONE() \
+#define ZEND_PARSE_PARAMETERS_NONE()  \
 	ZEND_PARSE_PARAMETERS_START(0, 0) \
 	ZEND_PARSE_PARAMETERS_END()
 #endif
+
+/* {{{ PHP_INI */
+PHP_INI_BEGIN()
+STD_PHP_INI_ENTRY("wcl.filter_blank", "0", PHP_INI_ALL, OnUpdateBool, filter_blank, zend_wcl_globals, wcl_globals)
+PHP_INI_END()
+/* }}} */
 
 /* {{{ void wcl_test1()
  */
@@ -34,8 +42,8 @@ PHP_FUNCTION(wcl_test2)
 	zend_string *retval;
 
 	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
+	Z_PARAM_OPTIONAL
+	Z_PARAM_STRING(var, var_len)
 	ZEND_PARSE_PARAMETERS_END();
 
 	retval = strpprintf(0, "Hello %s", var);
@@ -43,6 +51,51 @@ PHP_FUNCTION(wcl_test2)
 	RETURN_STR(retval);
 }
 /* }}}*/
+
+/* {{{ int wcl(string filepath)
+ */
+PHP_FUNCTION(wcl)
+{
+	char *filepath = NULL;
+	int argc = ZEND_NUM_ARGS();
+	size_t filepath_len;
+	char ch, pre = '\n';
+	FILE *fp;
+	zend_long lcount = 0;
+
+	if (zend_parse_parameters(argc, "s", &filepath, &filepath_len) == FAILURE)
+	{
+		return;
+	}
+
+	/* php_error(E_WARNING, "wcl: not yet implemented"); */
+
+	if ((fp = fopen(filepath, "r")) == NULL)
+	{
+		char *buf = NULL;
+		// php_error(E_WARNING, "wcl: Input file does not exist!");
+		php_error(E_WARNING, "wcl: Input file: %s does not exist!", filepath);
+		// fprintf(stderr, "Input file does not exist!\n");
+		// exit(1);
+	}
+	php_error(E_WARNING, "is filter: %ld", WCL_G(filter_blank));
+
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		if (ch == '\n')
+		{
+			if (WCL_G(filter_blank) && pre == ch)
+			{
+				continue;
+			}
+			lcount++;
+		}
+	}
+	fclose(fp);
+
+	RETURN_LONG(lcount);
+}
+/* }}} */
 
 /* {{{ PHP_RINIT_FUNCTION
  */
@@ -66,44 +119,57 @@ PHP_MINFO_FUNCTION(wcl)
 }
 /* }}} */
 
+PHP_MINIT_FUNCTION(wcl)
+{
+	REGISTER_INI_ENTRIES();
+
+	return SUCCESS;
+}
+
+PHP_MSHUTDOWN_FUNCTION(wcl)
+{
+	UNREGISTER_INI_ENTRIES();
+
+	return SUCCESS;
+}
+
 /* {{{ arginfo
  */
 ZEND_BEGIN_ARG_INFO(arginfo_wcl_test1, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_wcl_test2, 0)
-	ZEND_ARG_INFO(0, str)
+ZEND_ARG_INFO(0, str)
 ZEND_END_ARG_INFO()
 /* }}} */
 
 /* {{{ wcl_functions[]
  */
 static const zend_function_entry wcl_functions[] = {
-	PHP_FE(wcl_test1,		arginfo_wcl_test1)
-	PHP_FE(wcl_test2,		arginfo_wcl_test2)
-	PHP_FE_END
-};
+	PHP_FE(wcl_test1, arginfo_wcl_test1)
+		PHP_FE(wcl_test2, arginfo_wcl_test2)
+			PHP_FE(wcl, NULL)
+				PHP_FE_END};
 /* }}} */
 
 /* {{{ wcl_module_entry
  */
 zend_module_entry wcl_module_entry = {
 	STANDARD_MODULE_HEADER,
-	"wcl",					/* Extension name */
-	wcl_functions,			/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
-	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
-	PHP_RINIT(wcl),			/* PHP_RINIT - Request initialization */
-	NULL,							/* PHP_RSHUTDOWN - Request shutdown */
-	PHP_MINFO(wcl),			/* PHP_MINFO - Module info */
-	PHP_WCL_VERSION,		/* Version */
-	STANDARD_MODULE_PROPERTIES
-};
+	"wcl",				/* Extension name */
+	wcl_functions,		/* zend_function_entry */
+	PHP_MINIT(wcl),		/* PHP_MINIT - Module initialization */
+	PHP_MSHUTDOWN(wcl), /* PHP_MSHUTDOWN - Module shutdown */
+	PHP_RINIT(wcl),		/* PHP_RINIT - Request initialization */
+	NULL,				/* PHP_RSHUTDOWN - Request shutdown */
+	PHP_MINFO(wcl),		/* PHP_MINFO - Module info */
+	PHP_WCL_VERSION,	/* Version */
+	STANDARD_MODULE_PROPERTIES};
 /* }}} */
 
 #ifdef COMPILE_DL_WCL
-# ifdef ZTS
+#ifdef ZTS
 ZEND_TSRMLS_CACHE_DEFINE()
-# endif
+#endif
 ZEND_GET_MODULE(wcl)
 #endif
